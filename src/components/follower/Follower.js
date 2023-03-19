b36351aa0c06e73a73b2230e42ace2d4ebbb3547
Aman Verma <aman@Amans-MacBook-Air.local>
2023-03-20 00:12:09 +0530

    add some changes in follwers componets

--- src/components/follower/Follower.js
+++ src/components/follower/Follower.js
@@ -6,39 +6,41 @@ import { followAndUnfollowUser } from "../../redux/slices/feedSlice";
 import { useNavigate } from "react-router";
 
 function Follower({ user }) {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const feedData = useSelector((state) => state.feedDataReducer.feedData);
-    const [isFollowing, setIsFollowing] = useState();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const feedData = useSelector((state) => state.feedDataReducer.feedData);
+  const [isFollowing, setIsFollowing] = useState();
 
-    useEffect(() => {
-        setIsFollowing(
-            feedData.followings.find((item) => item._id === user._id)
-        );
-    }, [feedData]);
+  useEffect(() => {
+    setIsFollowing(feedData.followings.find((item) => item._id === user._id));
+  }, [feedData, user._id]);
 
-    function handleUserFollow () {
-        dispatch(followAndUnfollowUser({
-            userIdToFollow: user._id
-        }))
-    }
-
-    return (
-        <div className="Follower">
-            <div className="user-info" onClick={() => navigate(`/profile/${user._id}`)} >
-                <Avatar src={user?.avatar?.url} />
-                <h4 className="name">{user?.name}</h4>
-            </div>
-
-            <h5 onClick={handleUserFollow}
-                className={
-                    isFollowing ? "hover-link follow-link" : "btn-primary"
-                }
-            >
-                {isFollowing ? "Unfollow" : "Follow"}
-            </h5>
-        </div>
+  function handleUserFollow() {
+    dispatch(
+      followAndUnfollowUser({
+        userIdToFollow: user._id,
+      })
     );
+  }
+
+  return (
+    <div className="Follower">
+      <div
+        className="user-info"
+        onClick={() => navigate(`/profile/${user._id}`)}
+      >
+        <Avatar src={user?.avatar?.url} />
+        <h4 className="name">{user?.name}</h4>
+      </div>
+
+      <h5
+        onClick={handleUserFollow}
+        className={isFollowing ? "hover-link follow-link" : "btn-primary"}
+      >
+        {isFollowing ? "Unfollow" : "Follow"}
+      </h5>
+    </div>
+  );
 }
 
 export default Follower;
